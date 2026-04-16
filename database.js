1f66396493d1243f18c318e329a3aa427c2f4689
zunon21 <zunonserge10@gmail.com>
2026-04-16 17:10:13 +0000

    Use DATABASE_URL for Render

--- database.js
+++ database.js
@@ -1,16 +1,12 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        dialect: 'postgres',
-        logging: false
-    }
-);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  logging: false,
+  dialectOptions: {
+    ssl: process.env.NODE_ENV === 'production' ? { require: true, rejectUnauthorized: false } : false
+  }
+});
 
 module.exports = sequelize;
